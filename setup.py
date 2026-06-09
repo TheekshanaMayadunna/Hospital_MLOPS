b82bb19fb70954a2d502ca1f6e7c94e81480cdc2
TheekshanaMayadunna <110371608+TheekshanaMayadunna@users.noreply.github.com>
2026-06-09 18:05:35 +0530

    Update packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Hospital_MLOPS",
+    version="0.0.0",
+    author="Theekshana",
+    author_email="mayadunnatheekshana@gmail.com",
+    packages=find_packages()
+)
